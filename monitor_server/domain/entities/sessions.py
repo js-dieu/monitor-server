673bd364d25f01db4faa6b4d970ced0d13741518
Jean-Sebastien Dieu <dieu.jsebastien@yahoo.com>
2024-01-31 00:29:41 +0100

    feat(3): Added Session entity

---
+++ monitor_server/domain/entities/sessions.py
@@ -0,0 +1,37 @@
+import datetime
+import hashlib
+import typing as t
+from functools import cached_property
+
+from monitor_server.domain.entities.abc import Entity
+
+
+class Session(Entity):
+    uid: str
+    start_date: datetime.datetime
+    scm_revision: str
+    tags: t.Dict[str, t.Any]
+
+    @cached_property
+    def footprint(self) -> str:
+        if self.uid:
+            return self.uid
+        hr = hashlib.md5()
+        hr.update(self.start_date.isoformat().encode())
+        hr.update(self.scm_revision.encode())
+        hr.update(self.tags.get('description', '').encode())
+        return hr.hexdigest()
+
+    def __eq__(self, other: object) -> bool:
+        if type(other) is Session:
+            other_session = t.cast(Session, other)
+            return (
+                self.uid == other_session.uid
+                and self.start_date == other_session.start_date
+                and self.scm_revision == other_session.scm_revision
+                and self.tags.get('description', '') == other_session.tags.get('description', '')
+            )
+        return False
+
+    def __ne__(self, other: object) -> bool:
+        return not self == other
---
+++ monitor_server/tests/domain/entities/test_sessions.py
@@ -0,0 +1,50 @@
+import datetime
+
+import pytest
+
+from monitor_server.domain.entities.sessions import Session
+
+
+class TestSessionEntity:
+    def setup_method(self):
+        self.ref_session = Session(
+            uid='abcd',
+            scm_revision='e55dd4c2cee55e22c1e7388bf889b',
+            start_date=datetime.datetime(2024, 1, 31, 0, 20, 56, 1345, tzinfo=datetime.UTC),
+            tags={'description': 'a test session'},
+        )
+
+    def test_equality_returns_false_when_comparing_with_a_non_session_object(self):
+        result = self.ref_session == object()
+        assert not result
+
+    def test_equality_returns_true_when_comparing_two_objects_with_same_value(self):
+        other_session = Session.from_dict(self.ref_session.as_dict())
+        assert self.ref_session == other_session
+
+    @pytest.mark.parametrize('field_name', [field for field in Session.model_fields if field != 'tags'])
+    def test_equality_returns_false_when_two_objects_differs_only_by(self, field_name):
+        data = self.ref_session.as_dict()
+        if field_name == 'start_date':
+            data[field_name] = data[field_name] + datetime.timedelta(seconds=2)
+        else:
+            data[field_name] = data[field_name] * 2
+        other_session = Session.from_dict(data)
+        result = bool(self.ref_session == other_session)
+        assert not result
+
+    def test_equality_returns_false_when_two_session_only_differs_by_their_description(self):
+        data = self.ref_session.as_dict()
+        data['tags']['description'] = 'Another description'
+        other_session = Session.from_dict(data)
+        result = bool(self.ref_session == other_session)
+        assert not result
+
+    def test_that_footprint_returns_uid_if_provided(self):
+        assert self.ref_session.footprint == self.ref_session.uid
+
+    def test_that_footprint_computes_the_uid_when_not_given(self):
+        data = self.ref_session.as_dict()
+        data['uid'] = ''
+        other_machine = Session.from_dict(data)
+        assert other_machine.footprint
