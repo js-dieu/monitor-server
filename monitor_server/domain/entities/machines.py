546f6f2b2b3424c013755cbc98a46a90762831af
Jean-Sebastien Dieu <dieu.jsebastien@yahoo.com>
2024-02-13 11:28:19 +0100

    fix(9): Implement specfic method to compare two machines

--- monitor_server/domain/entities/machines.py
+++ monitor_server/domain/entities/machines.py
@@ -1,4 +1,5 @@
 import hashlib
+import typing as t
 from functools import cached_property
 
 from monitor_server.domain.entities.abc import Entity
@@ -16,6 +17,24 @@ class Machine(Entity):
     machine_arch: str
     system_info: str
     python_info: str
+
+    def __eq__(self, other: object) -> bool:
+        if type(other) is not Machine:
+            return False
+        other_machine = t.cast(Machine, other)
+        return (
+            self.uid == other_machine.uid
+            and self.cpu_frequency == other_machine.cpu_frequency
+            and self.cpu_vendor == other_machine.cpu_vendor
+            and self.cpu_count == other_machine.cpu_count
+            and self.cpu_type == other_machine.cpu_type
+            and self.total_ram == other_machine.total_ram
+            and self.hostname == other_machine.hostname
+            and self.machine_type == other_machine.machine_type
+            and self.machine_arch == other_machine.machine_arch
+            and self.system_info == other_machine.system_info
+            and self.python_info == other_machine.python_info
+        )
 
     @cached_property
     def footprint(self) -> str:
